4f29b3be093680f81a95b41ba005010cebc4adf7
Alhuin <GGe4rt3ln@>
2019-11-26 16:49:43 +0100

    Updates

---
+++ precache-manifest.22d51676331be048a8c5a2b8097e0971.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028bcd96a4d754fc5509463b60c8b2b4",
+    "url": "/Spotify/index.html"
+  },
+  {
+    "revision": "94ccb3629dee4e18a4c9",
+    "url": "/Spotify/static/css/main.246be130.chunk.css"
+  },
+  {
+    "revision": "3a0ed5d32a41448199b4",
+    "url": "/Spotify/static/js/2.8472fd3f.chunk.js"
+  },
+  {
+    "revision": "94ccb3629dee4e18a4c9",
+    "url": "/Spotify/static/js/main.54641d8a.chunk.js"
+  },
+  {
+    "revision": "93948101f8b142415aa9",
+    "url": "/Spotify/static/js/runtime-main.e04bae9b.js"
+  }
+]);
