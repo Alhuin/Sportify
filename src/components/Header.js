afc9b735773da510d7b1425754944f579e0fef24
Alhuin <GGe4rt3ln@>
2019-11-26 16:42:21 +0100

    publish on github

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import '../index.css';
 
 class Header extends Component {
+
+
   render() {
     const { type, name, lastName, membership } = this.props;
     let content = "";
-
+    let headerColor = {background: "linear-gradient(to bottom right, #3D4FFF, #7CBBFF)"};
     // On choisit le contenu du Header en fonction de la page
 
     switch (type) {
@@ -24,13 +26,14 @@ class Header extends Component {
           <p>{name} {lastName}</p>
           <p style={{fontSize: 12}}>Offre {membership} à des fins de démonstration.</p>
         </div>;
+        // headerColor = {background: "linear-gradient(to bottom right, yellow, green)"};
         break;
       default:
         content = "Sportify";
     }
 
     return(
-      <div className="HeaderContainer">
+      <div className="HeaderContainer" style={headerColor}>
         {content}
       </div>
     );
